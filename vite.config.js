5b58be32d9f1d83c873f7eb38d0f45d83c476d13
alejandrospainschool-cloud <alejandrospainschool@gmail.com>
2025-12-19 05:04:09 +0000

    Day 2 3 changes

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://lenguahubai.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
